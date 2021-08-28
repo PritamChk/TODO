59f67b5878f856806305420804314a6a7654fa54
Rivu Pal <rivupal03@gmail.com>
2021-08-28 18:09:49 +0530

    User class created

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    user_name = models.CharField(max_length=150, unique=True)
+    password = models.CharField(max_length=15)
+    phone_no = models.CharField(max_length=12)
+    date_of_joining = models.DateTimeField(auto_now_add=True)
+    
+
+
