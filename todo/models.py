428eefd0b3ce4b1241d4f7065d94a9d7d7531b1e
Rivu Pal <rivupal03@gmail.com>
2021-08-29 11:01:01 +0530

    password size change

--- todo/models.py
+++ todo/models.py
@@ -15,7 +15,7 @@ class User(models.Model):
     first_name = models.CharField(max_length=150)
     last_name = models.CharField(max_length=150)
     user_name = models.CharField(max_length=150, unique=True)
-    password = models.CharField(max_length=15)
+    password = models.CharField(max_length=150)
     gender = models.CharField( max_length=2, choices=USER_SEX, default=USER_SEX_MALE)
     phone_no = models.CharField(max_length=12)
     date_of_joining = models.DateTimeField(auto_now_add=True)
