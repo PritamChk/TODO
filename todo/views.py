db9fbe46bdbf899a4ecb313cecd5773e33c4705e
Rivu Pal <rivupal03@gmail.com>
2021-09-03 12:42:54 +0530

    sign up logic done

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import User
 
 def login(request):
     return render(request,"login.html")
@@ -6,13 +7,23 @@ def login(request):
 def Registration(request):
     if request.method == "POST":
         first_name = request.POST["first_name"]
-        first_name = request.POST["first_name"]
         last_name = request.POST["last_name"]
         user_name = request.POST["user_name"]
         password = request.POST["password"]
         phone = request.POST["phone"]
         sex = request.POST["sex"]
-    
-        return render(request,"home.html", {"details":[first_name,last_name,user_name,password,phone,sex]})
+        user_exists=User.objects.filter(user_name = user_name).exists()
+        if user_exists:
+            return render(request,"home.html", {"details":[first_name,last_name,user_name,password,phone,sex]})
+        else:
+            user=User()
+            user.first_name=first_name
+            user.last_name=last_name
+            user.user_name=user_name
+            user.password=password
+            user.phone_no=phone
+            user.gender=sex
+            user.save()
+            return render(request,"home.html", {"details":[first_name,last_name,user_name,password,phone,sex]})
     else:
         return render(request,"login.html")
